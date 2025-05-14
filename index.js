06ed0a8157afb8901f926775f473d70581c65353
mayur wagh <mswagh98@gmail.com>
2025-05-15 04:33:47 +0530

    refactor: rewrite multiply function to use currying with closure
    The multiply function was refactored to use closure-based currying instead of the  method. This improves code readability and flexibility by allowing partial application of arguments. The documentation in  was also updated to reflect the new implementation and include additional examples.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-let multiply = function (x, y) {
-  console.log(x * y);
+let multiply = function (x) {
+  return function (y) {
+    console.log(x * y);
+  };
 };
 
-let multiplyByTwo = multiply.bind(this, 2);
-multiplyByTwo(3);
+let multiplyByTwo = multiply(2);
+multiplyByTwo(5);
